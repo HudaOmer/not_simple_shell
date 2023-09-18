bef0aa000c6516f880aec6cb2660fb00d4236b9f
Ethar <etharabdelazeem1@gmail.com>
2023-09-18 14:15:56 +0300

    Update main.h

--- main.h
+++ main.h
@@ -15,6 +15,12 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
 /* string.c functions */
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
